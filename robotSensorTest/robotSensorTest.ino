5af905b8ce717201e16e1671c2f4ce43a1840d93
Ine Lofthus <inelofthus@gmail.com>
2017-03-30 09:33:55 +0200

    Border detect

--- robotSensorTest/robotSensorTest.ino
+++ robotSensorTest/robotSensorTest.ino
@@ -19,6 +19,41 @@
 #include <ZumoMotors.h>
 #include <Pushbutton.h>
 
+
+
+
+
+/*
+Drive forward and turn left or right when border is detected
+  - Only reflectionsensor 0 and 5 are used.
+*/
+#include <ZumoMotors.h>
+#include <Pushbutton.h>
+#include <QTRSensors.h>
+#include <ZumoReflectanceSensorArray.h>
+ 
+#define LED 13
+ 
+// this might need to be tuned for different lighting conditions, surfaces, etc.
+#define QTR_THRESHOLD  1800 // 
+  
+// these might need to be tuned for different motor types
+#define REVERSE_SPEED     200 // 0 is stopped, 400 is full speed
+#define TURN_SPEED        200
+#define FORWARD_SPEED     200
+#define REVERSE_DURATION  200 // ms
+#define TURN_DURATION     200 // ms
+
+ 
+#define NUM_SENSORS 6
+unsigned int sensor_values[NUM_SENSORS];
+ 
+ZumoReflectanceSensorArray sensors;
+
+
+
+
+
 //ZumoReflectanceSensorArray reflectanceSensors;
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON);
@@ -66,6 +101,9 @@ void setup()
   servo.attach(servoPin);
   servo.write(20);*/
   button.waitForButton();
+
+
+  sensors.init();
 }
 
 void alignForPotentialMurder(int& LSpeed, int& RSpeed, float distance){
@@ -135,9 +173,37 @@ void loop()
   float distanceL = sonarL.ping_cm();
   //Serial.println(String(distanceL)+"\t"+String(distanceR));
 
-  search(LSpeed,RSpeed);
-  
+  //search(LSpeed,RSpeed);
   //motors.setSpeeds(LSpeed,RSpeed);
+
+  sensors.read(sensor_values);
+
+  if (sensor_values[0] < QTR_THRESHOLD)
+  {
+    // if leftmost sensor detects line, reverse and turn to the right
+    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+    delay(REVERSE_DURATION);
+    motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+    delay(TURN_DURATION);
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
+  else if (sensor_values[5] < QTR_THRESHOLD)
+  {
+    // if rightmost sensor detects line, reverse and turn to the left
+    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+    delay(REVERSE_DURATION);
+    motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+    delay(TURN_DURATION);
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
+  else
+  {
+    // otherwise, go straight
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
+
+  
+
   
   delay(40);
 }
